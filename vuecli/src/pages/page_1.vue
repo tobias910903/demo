982594e8d0fecd2ccf0fd131826a5f26e452d85a
lihuyong <1003957188@qq.com>
2019-05-30 19:44:05 +0800

    update

--- vuecli/src/pages/page_1.vue
+++ vuecli/src/pages/page_1.vue
@@ -8,13 +8,28 @@
 </template>
 
 <script>
+import Vue from 'vue'
+import {mapState, mapActions, mapGetters} from 'vuex'
+import {timeFun} from '@/utils/time' // 引入自定义的JS方法 
+Vue.prototype.$time = timeFun
+
+
 export default {
-	data () {
+	data(){
 		return {
 			msg: '页面一'
 		}
 	},
-	mounted: function(){
+	computed:{
+		...mapState({
+			count_rewrite: state => state.count
+		})
+	},
+	mounted(){
+		this.$time.bb(); // 自定义一个JS
+		console.log(this.count_rewrite);
+		console.log(this.$store.state.count);
+
 		console.log(this);
 		console.log(this.$route.params);
 	}
---
+++ vuecli/src/utils/time.js
@@ -0,0 +1,10 @@
+let timeFun = {
+	aa: function(){
+		console.log(123);
+	},
+	bb: function(){
+		console.log(456);
+	}
+}
+
+export {timeFun}
